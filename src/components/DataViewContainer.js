b851810eb9aff91092c53068e491d850f7dddbee
FayeZ <im5dii@gmail.com>
2018-01-31 20:14:08 -0800

    add slider

---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ShotChart } from './ShotChart';
+import {CountSlider} from './CountSlider'
+
+export class DataViewContainer extends React.Component {
+    state = {
+        inputValue: 1,
+    }
+    onChange = (value) => {
+        this.setState({
+            inputValue: value,
+        });
+    }
+    render() {
+        return(
+            <div className="data-view">
+                <ShotChart playerId={ this.props.playerId }/>
+                <div className="filters">
+                   <CountSlider/>
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/ShotChart.js
+++ src/components/ShotChart.js
@@ -4,12 +4,15 @@ import * as d3 from 'd3';
 import { hexbin } from 'd3-hexbin';
 import { court, shots } from 'd3-shotchart';
 import PropTypes from 'prop-types';
+
 window.d3_hexbin = {hexbin : hexbin}; // workaround library problem
+
 export class ShotChart extends React.Component {
     static propTypes = {
-        playerId: PropTypes.number.isRequired,
+        playerId: PropTypes.number,
     }
-    componentDidMount() {
+
+    componentDidUpdate() {
         nba.stats.shots({
             PlayerID: this.props.playerId
         }).then((response) => {
@@ -20,7 +23,9 @@ export class ShotChart extends React.Component {
                 shot_distance: shot.shotDistance,
                 shot_made_flag: shot.shotMadeFlag,
             }));
+
             const courtSelection = d3.select("#shot-chart");
+            courtSelection.html('');
             const chart_court = court().width(500);
             const chart_shots = shots().shotRenderThreshold(2).displayToolTips(true).displayType("hexbin");
             courtSelection.call(chart_court);
